bca4389e8936f8d1e62eee65b0b3c7ceb4b2cd83
Vincent de La Fonchais <vdelafonchais@itnetwork.fr>
2022-02-16 21:14:16 +0100

    Fix responsive burger menu feature (no styles for the time being)

--- header.php
+++ header.php
@@ -7,7 +7,7 @@
 <head>
 <meta charset="<?php bloginfo( 'charset' ); ?>" />
 <meta name="viewport" content="width=device-width" />
-<title><?php wp_title( '|', true, 'right' ); ?></title>
+<title><?php wp_title( '|', true, 'right' ); ?> <?php bloginfo( 'name' ); ?></title>
 <link rel="profile" href="https://gmpg.org/xfn/11" />
 <link rel="pingback" href="<?php echo esc_url( get_bloginfo( 'pingback_url' ) ); ?>">
 <?php wp_head(); ?>
@@ -15,20 +15,41 @@
 
 <body <?php body_class(); ?>>
 <?php wp_body_open(); ?>
-<div id="page" class="hfeed site">
+<div id="page" class="site">
 	<header id="masthead" class="site-header">
 		<div class="ui-width">
-			<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
-			<h2 class="site-description"><?php bloginfo( 'description' ); ?></h2>
+			<?php if(is_front_page()): ?>
+				<h1 class="site-title">
+			<?php else: ?>
+				<p class="site-title">
+			<?php endif; ?>
+				<a href="<?php echo esc_url( home_url( '/' ) ); ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a>
+			<?php if(is_front_page()): ?>
+				</h1>
+			<?php else: ?>
+				</p>
+			<?php endif; ?>
+			<?php if(is_front_page()): ?>
+				<h2 class="site-description">
+			<?php else: ?>
+				<p class="site-description">
+			<?php endif; ?>
+				<?php bloginfo( 'description' ); ?>
+			<?php if(is_front_page()): ?>
+				</h2>
+			<?php else: ?>
+				</p>
+			<?php endif; ?>
 			
 			<nav id="site-navigation" class="main-navigation">
 				<button class="menu-toggle"><?php _e( 'Menu', 'webpack' ); ?></button>
-				<a class="assistive-text" href="#content" title="<?php esc_attr_e( 'Skip to content', 'webpack' ); ?>"><?php _e( 'Skip to content', 'webpack' ); ?></a>
+				<a class="visually-hidden" href="#content" title="<?php esc_attr_e( 'Skip to content', 'webpack' ); ?>"><?php _e( 'Skip to content', 'webpack' ); ?></a>
 				<?php
 				wp_nav_menu(
 					array(
-						'theme_location' => 'primary',
+						'menu' => 'Primary menu',
 						'menu_class'     => 'nav-menu',
+						'container'			 => 'ul',
 					)
 				);
 				?>
